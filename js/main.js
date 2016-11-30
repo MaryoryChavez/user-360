3e54dc1218319d73b72e8c7d965d71e5e0c4a247
MaryoryChavez <maryory.chavez@gmail.com>
2016-11-30 15:05:01 -0500

    javascript solucion a mostrar

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-/*How to make the hero section always fill browser window?*/
-$(window).resize(function() {
-        $('#hero').height($(window).height());
-}).resize();
+/*--------------Desplasamiento Menu----------*/
 
 $('.nave').on('click',function(){
 	$('nav').toggleClass('mostrar');
 })
-/*sticky*/
-var altura = $('header').offset().top;
+
+
+/*--------------Menu----------*/
+var altura = $('.header').offset().top;
 
 $(window).on('scroll', function(){
 	if ( $(window).scrollTop() > altura ){
-		$('header').addClass('menu-fixed');
+		$('.header').addClass('menu-fixed');
 	} else {
-		$('header').removeClass('menu-fixed');
+		$('.header').removeClass('menu-fixed');
 	}
 });
